aec59ad2bce7ec5d2c735f6ec271ad01546cccd9
Juan Pedro Bolivar Puente <raskolnikov@gnu.org>
2017-08-02 16:14:03 +0200

    Still show feedback while asynchronous effects are finishing
    Before, we sould just call stop() on the io_service to cause the
    application to stop.  This was not great, because it would cause the
    io_service to disregard any remaining events in the queue, and no new
    events would be processed.  This means that we would not get any
    feedback from the remaining tasts (lets say we are saving a big file
    during exit) and potentially chains of effect->update->effect could be
    aborted in between.
    
    Now, to quite, we just stop reading from the terminal, and wait until
    the io_service runs out of work.  To prevent it from running out of
    work too early, we keep a io_service::work object inside the thread of
    asynchronous tasks.
    
    The API also has been streamlined, so everything that an effect might
    want to know about is kept in a context<Action> object.  For ease of
    use, this object is copyable, plus we can convert between them as long
    as their Action objects are compatible.  This last aspect is important
    for easily composing effects with action subtypes, a pattern we were
    already using (see how update() delegates in update_buffer for
    applying buffer actions).

--- store.hpp
+++ store.hpp
@@ -20,21 +20,54 @@
 
 #pragma once
 
-#include <ewig/io_service.hpp>
+#include <boost/asio/io_service.hpp>
+
 #include <functional>
+#include <future>
 
 namespace ewig {
 
 template <typename Action>
-using dispatcher = std::function<void(Action)>;
+struct context
+{
+    using action_t      = Action;
+    using service_t     = boost::asio::io_service;
+    using service_ref_t = std::reference_wrapper<service_t>;
+    using finish_t      = std::function<void()>;
+    using dispatch_t    = std::function<void(action_t)>;
+
+    std::reference_wrapper<service_t> service;
+    finish_t finish;
+    dispatch_t dispatch;
+
+    context(const context& ctx) = default;
+    context(context&& ctx) = default;
+
+    template <typename Action_>
+    context(const context<Action_>& ctx)
+        : service(ctx.service)
+        , finish(ctx.finish)
+        , dispatch(ctx.dispatch)
+    {}
+
+    context(service_t& serv, finish_t fn, dispatch_t ds)
+        : service(serv)
+        , finish(std::move(fn))
+        , dispatch(std::move(ds))
+    {}
+
+    template <typename Fn>
+    void async(Fn&& fn) const
+    {
+        std::thread([fn=std::move(fn),
+                   work=boost::asio::io_service::work(service)] {
+            fn();
+        }).detach();
+    }
+};
 
 template <typename Action>
-using effect = std::function<void(io_service&, dispatcher<Action>)>;
-
-constexpr auto do_stop = [] (auto& serv, auto&&)
-{
-    serv.stop();
-};
+using effect = std::function<void(const context<Action>&)>;
 
 constexpr auto noop = [] (auto&&...) {};
 
@@ -47,35 +80,45 @@ struct result : std::pair<Model, effect<Action>>
 };
 
 template <typename Model, typename Action>
-struct store
+struct store : context<Action>
 {
+    using base_t    = context<Action>;
     using model_t   = Model;
     using action_t  = Action;
+    using finish_t  = typename base_t::finish_t;
     using reducer_t = std::function<result<model_t, action_t>
                                    (model_t, action_t)>;
     using view_t    = std::function<void(model_t)>;
 
-    store(io_service& serv, model_t init, reducer_t reducer, view_t view)
-        : serv_(serv)
-        , model_(std::move(init))
-        , reducer_(std::move(reducer))
-        , view_(std::move(view))
+    store(const store&) = delete;
+    store& operator=(const store&) = delete;
+
+    store(boost::asio::io_service& serv,
+          model_t init,
+          reducer_t reducer,
+          view_t view,
+          finish_t finish)
+        : base_t{serv,
+                 std::move(finish),
+                 [this] (auto ev) { dispatch(ev); }}
+        , model_{std::move(init)}
+        , reducer_{std::move(reducer)}
+        , view_{std::move(view)}
     {
         view_(model_);
     }
 
     void dispatch(action_t action)
     {
-        serv_.post([=] {
+        base_t::service.get().post([=] {
             auto [model, effect] = reducer_(model_, action);
             model_ = model;
-            effect(serv_, [&] (auto x) { dispatch(x); });
+            effect(*this);
             view_(model_);
         });
     }
 
 private:
-    io_service& serv_;
     model_t model_;
     reducer_t reducer_;
     view_t view_;
