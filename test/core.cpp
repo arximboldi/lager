4c4e52111f2567cac9aa78c5af408aa798141436
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2017-11-03 13:39:35 +0100

    Change license to MIT

--- test/core.cpp
+++ test/core.cpp
@@ -5,17 +5,9 @@
 // This file is part of lager.
 //
 // lager is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// lager is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with lager.  If not, see <http://www.gnu.org/licenses/>.
+// it under the terms of the MIT License, as detailed in the LICENSE
+// file located at the root of this source code distribution,
+// or here: <https://github.com/arximboldi/lager/blob/master/LICENSE>
 //
 
 #include <catch.hpp>
