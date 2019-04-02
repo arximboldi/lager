61d83575f8eab3132266e6b178e7d239f48ebbbe
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2019-04-02 16:55:07 +0200

    Add minimalistic queue event loop

---
+++ lager/event_loop/queue.hpp
@@ -0,0 +1,57 @@
+//
+// lager - library for functional interactive c++ programs
+// Copyright (C) 2019 Juan Pedro Bolivar Puente
+//
+// This file is part of lager.
+//
+// lager is free software: you can redistribute it and/or modify
+// it under the terms of the MIT License, as detailed in the LICENSE
+// file located at the root of this source code distribution,
+// or here: <https://github.com/arximboldi/lager/blob/master/LICENSE>
+//
+
+#pragma once
+
+#include <utility>
+
+namespace lager {
+
+struct queue_event_loop
+{
+    using event_fn = std::function<void()>;
+
+    void post(event_fn ev) { queue_.push_back(std::move(ev)); }
+    void finish() { throw std::logic_error{"not implemented!"}; }
+    void pause() { throw std::logic_error{"not implemented!"}; }
+    void resume() { throw std::logic_error{"not implemented!"}; }
+    template <typename Fn>
+    void async(Fn&& fn) { throw std::logic_error{"not implemented!"}; }
+
+    void step()
+    {
+        auto is_root = !queue_.empty();
+        if (is_root) {
+            for (auto i = std::size_t{}; i < queue_.size(); ++i)
+                queue_[i]();
+            queue_.clear();
+        }
+    }
+
+private:
+    std::vector<event_fn> queue_;
+};
+
+struct with_queue_event_loop
+{
+    std::reference_wrapper<queue_event_loop> loop;
+
+    template <typename Fn>
+    void async(Fn&& fn) { loop.get().async(std::forward<Fn>(fn)); }
+    template <typename Fn>
+    void post(Fn&& fn) { loop.get().post(std::forward<Fn>(fn)); }
+    void finish() { loop.get().finish(); }
+    void pause() { loop.get().pause(); }
+    void resume() { loop.get().resume(); }
+};
+
+}  // namespace lager
