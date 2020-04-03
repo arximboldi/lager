db1dd8ff228e4ddf01c3bf8d1e51ac7ec531df66
Katherine Miller <khmiller@globusmedical.com>
2020-04-03 10:42:38 -0400

    Fix todo qml example.

--- example/todo/qml/main.cpp
+++ example/todo/qml/main.cpp
@@ -65,7 +65,7 @@ public:
     Q_INVOKABLE Item* todo(int index)
     {
         return new Item{
-            state_[&todo::model::todos][index][lager::lens::fallback()]};
+            state_[&todo::model::todos][index][lager::lenses::value_or()]};
     }
 
     Q_INVOKABLE void add(QString text)
