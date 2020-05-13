b4850ff44a7926f2a6f00af065210873fbbfeb92
jarzec <jarek.glowacki@gmail.com>
2020-05-13 22:00:21 +0200

    Add async() to with_qt_event_loop
    - Modify with_qt_event_loop to implement async() with possibility
    to specify a QThreadPool, while maintaining backward compatibility
    - Add a respective test

--- lager/event_loop/qt.hpp
+++ lager/event_loop/qt.hpp
@@ -12,29 +12,54 @@
 
 #pragma once
 
+#include <lager/util.hpp>
+
 #include <QCoreApplication>
 #include <QMetaObject>
+#include <QtConcurrent>
+#include <QThreadPool>
 
 #include <functional>
 #include <utility>
+#include <variant>
 
 namespace lager {
 
 struct with_qt_event_loop
 {
-    std::reference_wrapper<QObject> obj;
+    using qt_obj_ref_variant_t = std::variant<std::reference_wrapper<QObject>,
+                                              std::reference_wrapper<QThreadPool>>;
+    qt_obj_ref_variant_t obj_ref_variant;
+
+    static QThreadPool *get_thread_pool_ptr(qt_obj_ref_variant_t obj_ref_variant) {
+            return std::visit(lager::visitor{
+                       [](std::reference_wrapper<QObject>) {
+                           return QThreadPool::globalInstance();
+                       },
+                       [](std::reference_wrapper<QThreadPool> thread_pool) {
+                           return &thread_pool.get();
+                       }
+                },
+                obj_ref_variant);
+        }
+
+    ~with_qt_event_loop() {
+        get_thread_pool_ptr(obj_ref_variant)->waitForDone();
+    }
 
     template <typename Fn>
     void async(Fn&& fn)
     {
-        throw std::runtime_error{"not implemented!"};
+        auto thread_pool_ptr = get_thread_pool_ptr(obj_ref_variant);
+        QtConcurrent::run(thread_pool_ptr, std::forward<Fn>(fn));
     }
 
     template <typename Fn>
     void post(Fn&& fn)
     {
+        auto obj_ptr = get_thread_pool_ptr(obj_ref_variant);
         QMetaObject::invokeMethod(
-            &obj.get(), std::forward<Fn>(fn), Qt::QueuedConnection);
+            obj_ptr, std::forward<Fn>(fn), Qt::QueuedConnection);
     }
 
     void finish() { QCoreApplication::instance()->quit(); }
