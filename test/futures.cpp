5ae6bdc3d9be2f57577a0f45cee84a354b4c9cc2
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2023-01-10 22:13:15 +0100

    Add more futures tests

--- test/futures.cpp
+++ test/futures.cpp
@@ -66,6 +66,69 @@ TEST_CASE("future then callback is called after effects")
     CHECK(called == 1);
 }
 
+TEST_CASE("future effect callback chaining")
+{
+    auto effect1 = [&](auto&& ctx) -> lager::future { return ctx.dispatch(1); };
+    auto effect2 = [&](auto&& ctx) -> lager::future { return ctx.dispatch(2); };
+
+    auto queue = lager::queue_event_loop{};
+    auto store = lager::make_store<int>(
+        0,
+        lager::with_queue_event_loop{queue},
+        lager::with_futures,
+        lager::with_reducer([&](int s, int a) -> lager::result<int, int> {
+            if (a == 0)
+                return {s, effect1};
+            else if (a == 1)
+                return {s + a, effect2};
+            else
+                return s + a;
+        }));
+
+    auto called = 0;
+    store.dispatch(0).then([&] {
+        CHECK(*store == 3);
+        ++called;
+    });
+    CHECK(called == 0);
+
+    queue.step();
+    CHECK(called == 1);
+}
+
+TEST_CASE("future effect callback chaining and stupid effect")
+{
+    auto effect0 = [&](auto&& ctx) -> void {};
+    auto effect1 = [&](auto&& ctx) -> lager::future { return ctx.dispatch(1); };
+    auto effect2 = [&](auto&& ctx) -> lager::future { return ctx.dispatch(2); };
+
+    auto queue = lager::queue_event_loop{};
+    auto store = lager::make_store<int>(
+        0,
+        lager::with_queue_event_loop{queue},
+        lager::with_futures,
+        lager::with_reducer([&](int s, int a) -> lager::result<int, int> {
+            if (a == 0)
+                return {s,
+                        lager::sequence(lager::effect<int>{effect1},
+                                        lager::effect<int>{effect0})};
+            else if (a == 1)
+                return {s + a, effect2};
+            else
+                return s + a;
+        }));
+
+    auto called = 0;
+    store.dispatch(0).then([&] {
+        CHECK(*store == 3);
+        ++called;
+    });
+    CHECK(called == 0);
+
+    queue.step();
+    CHECK(called == 1);
+}
+
 TEST_CASE("combining future")
 {
     auto queue = lager::queue_event_loop{};
